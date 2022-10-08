e58b87f21c9f2863aafdc98322f78bacaab2b3d4
Gabi Cruz <gabrielasnts30@gmail.com>
2022-10-07 23:31:35 -0300

    splash screen initial structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
+import React from 'react';
+
 function App() {
-  return (
-    <div className="bg-red-500">
-      aaa
-    </div>
-  )
+	return (
+		<div
+			className='bg-red-700 h-screen text-white flex flex-col space-y-4 p-5'
+		>
+			<h1 className='text-2xl text-center font-bold'>
+				Dê o seu palpite na Copa do Mundo do Catar 2022! 
+			</h1>
+			<button
+				className='text-red-700 bg-white px-8 py-3 rounded-2xl font-bold'
+			>
+				Criar minha conta
+			</button>
+			<button
+				className='text-white border border-white px-8 py-3 rounded-2xl font-bold'
+			>
+				Fazer login
+			</button>
+		</div>
+	);
 }
 
-export default App
+export default App;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,35 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      fontSize: {
-        xs: ['12px', '16px'],
-        sm: ['14px', '18px'],
-        base: ['16px', '24px'],
-        lg: ['20px', '28px'],
-        xl: ['24px', '32px'],
-        '2xl': ['32px', '40px'],
-        '3xl': ['48x', '56px'],
-      },
-      colors: {
-        black: '#300219',
-        white: '#F4F6FF',
-        red: {
-          300: '#BB2E57',
-          500: '#AF053F',
-          700: '#300219',
-        },
-        grey: {
-          300: '#B1B4BD',
-          500: '#91949D',
-          700: '#695C74'
-        }
-      }
-    },
-  },
-  plugins: [],
-}
+	content: [
+		'./index.html',
+		'./src/**/*.{js,ts,jsx,tsx}',
+	],
+	theme: {
+		extend: {
+			fontSize: {
+				xs: ['12px', '16px'],
+				sm: ['14px', '18px'],
+				base: ['16px', '24px'],
+				lg: ['20px', '28px'],
+				xl: ['24px', '32px'],
+				'2xl': ['32px', '40px'],
+				'3xl': ['48px', '56px'],
+			},
+			colors: {
+				black: '#300219',
+				white: '#F4F6FF',
+				red: {
+					300: '#BB2E57',
+					500: '#AF053F',
+					700: '#300219',
+				},
+				grey: {
+					300: '#B1B4BD',
+					500: '#91949D',
+					700: '#695C74'
+				}
+			}
+		},
+	},
+	plugins: [],
+};
